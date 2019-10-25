08d0e6add538fcb5aec180483ab6655484e71aa6
JulianQA <54037880+JulianQA@users.noreply.github.com>
2019-10-25 08:49:56 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -13,6 +13,14 @@ class TestmyFunctions(unittest.TestCase):
         esperado = -0.9999999999999998
         res = partilinea.trans(a,b)
         self.assertEqual(esperado,res)
+    def testaverage(self):
+        a = [[(1,0),(0,-1)],
+             [(0,1),(2,0)]]
+        b = [[(0.7071067811865476,0)],
+             [(0,0.7071067811865476)]]
+        esperado = -0.5
+        res = partilinea.average(a,b)
+        self.assertEqual(esperado,res)
 if __name__ == '__main__':
     unittest.main()
         
